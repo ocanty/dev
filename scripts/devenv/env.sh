8846088617bfff5ef9bb2724bbf5064c36776a48
ocanty <oisin@canty.dev>
2022-10-02 02:40:48 +0100

    Mark apt as executable + node env fix

--- scripts/devenv/env.sh
+++ scripts/devenv/env.sh
@@ -7,16 +7,14 @@ export PATH="$DEV_ROOT/vendor/go/bin:$PATH"
 export PATH="$DEV_ROOT/vendor/tmux:$PATH"
 export GOPATH="$DEV_ROOT/vendor/go"
 
-
 . "$NVM_DIR/nvm.sh"
 . "$NVM_DIR/bash_completion"
 
-yarn set version berry
+yarn set version berry || true
 
 nvm use > /dev/null
 
 export PATH="$DEV_ROOT/node_modules/.bin:$PATH"
-# export NODE_PATH="$DEV_ROOT/vendor/node_modules:$NODE_PATH"
 
 . "$DEV_ROOT/vendor/python3/bin/activate"
 
